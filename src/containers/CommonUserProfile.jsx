bd844e9a595102c4f397756da50a5974de784238
Krituraj Anand <jaiswalkrituraj20@gmail.com>
2022-06-24 23:58:10 +0530

    feat: created Common user profile page

--- src/containers/CommonUserProfile.jsx
+++ src/containers/CommonUserProfile.jsx
@@ -1,6 +1,35 @@
-
+import { Profile, Spinner } from "components";
+import { useParams } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { followUser, unfollowUser } from "services";
 export function CommonUserProfile() {
-  return(
-    <h1>THis is user profile page for other users</h1>
-  )
+  const dispatch = useDispatch();
+  const { username } = useParams();
+  const { status, allUsers, currentUser } = useSelector((state) => state.user);
+  const isPageLoading = status.value === "pending" && status.type === "getAllUsers";
+  let user;
+  let isUserFollowed;
+  if (!isPageLoading) {
+    user = allUsers.find((user) => user.username === username) ?? {};
+    isUserFollowed = currentUser.following?.some((id) => id === user._id);
+  }
+  const followUnfollowUser = () => {
+    if (!isUserFollowed) {
+      dispatch(followUser(user._id));
+      return;
+    }
+    dispatch(unfollowUser(user._id));
+  };
+
+
+  return isPageLoading ? (
+    <Spinner/>
+  ) : (
+    <Profile
+      {...user}
+      isCurrentUserProfile={false}
+      isUserFollowed={isUserFollowed}
+      followUnfollowUser={followUnfollowUser}
+    />
+  );
 }
